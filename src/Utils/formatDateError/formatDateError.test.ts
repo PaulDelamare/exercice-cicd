20976d4cb46ecd1eab9f54d5d2dff0c563c1f017
Paul_Delamare <paulodela2006@gmail.com>
2025-11-04 14:10:33 +0100

    Corriger les tests formatDate pour UTC

--- src/Utils/formatDateError/formatDateError.test.ts
+++ src/Utils/formatDateError/formatDateError.test.ts
@@ -6,18 +6,18 @@ describe('formatDate', () => {
     it('formats the date correctly', () => {
         const date = new Date('2024-11-05T11:01:00Z');
         const formattedDate = formatDate(date);
-        expect(formattedDate).toBe('05/11/2024 - 12h01');
+        expect(formattedDate).toBe('05/11/2024 - 11h01');
     });
 
     it('handles single digit day and month correctly', () => {
         const date = new Date('2024-01-05T01:01:00Z');
         const formattedDate = formatDate(date);
-        expect(formattedDate).toBe('05/01/2024 - 02h01');
+        expect(formattedDate).toBe('05/01/2024 - 01h01');
     });
 
     it('handles single digit hours and minutes correctly', () => {
         const date = new Date('2024-11-05T01:01:00Z');
         const formattedDate = formatDate(date);
-        expect(formattedDate).toBe('05/11/2024 - 02h01');
+        expect(formattedDate).toBe('05/11/2024 - 01h01');
     });
 });
